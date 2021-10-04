77f6ec02227d3059952103c5eb144f77d731069a
ogus02 <ogus0247@gmail.com>
2021-10-04 11:21:14 +0200

    changed directory and handling response request

--- server.js
+++ server.js
@@ -2,33 +2,16 @@
 
 const express = require('express');
 const fs = require('fs');
-
 const app = express();
 const PORT = process.env.PORT || 8042;
 
-app.get('/', (req, res) => {
+app.use(express.static('public'));
 
+app.get('/', (req, res) => {
     fs.readFile('index.html', (err, data) => {
         res.setHeader('Content-Type', 'text/html');
         res.send(data);
     });
-
-});
-
-app.get('/stylesheet.css', (req, res) => {
-
-    fs.readFile('stylesheet.css', (err, data) => {
-        res.send(data);
-    });
-
-});
-
-app.get('/images/oscar.png', (req, res) => {
-
-    fs.readFile('images/oscar.png', (err, data) => {
-        res.send(data);
-    });
-
 });
 
 app.listen(PORT, () => {
