3e5afe9395254ed969ef39fbcabcbd7d8266dbc3
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-12-12 21:32:24 +0100

    generated pug files, nodejs, express

--- public/js/main.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    const change = document.getElementById("burger-links");
-    if (change.style.display === "block") {
-        change.style.display = "none";
-    } else {
-        change.style.display = "block";
-    }
-}
