bb80cfae8af24947a09baead35cbe301c63f68d7
Kasiem Saeed <kasiemsaeed@gmail.com>
2022-01-13 08:38:12 +0100

    testing navbar

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,4 +4,7 @@ console.log('Main.js is alive')
 
 window.onload = () => {
     console.log('This is onload');
+    const href = window.location.href;
+    const nav = document.createElement('nav');
+    // nav.classList.add("navbar navbar-expand-md navbar-light bg-light");
 }
